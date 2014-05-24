d570cc75d39467a5c6577c3f75ac1021377dee14
Laurent Cozic <laurent@pogopixels.com>
2014-05-24 18:26:28 -0300

    QxTrash - Added support for Windows

--- plugins/MvDeleteFile/qxtrash.cpp
+++ plugins/MvDeleteFile/qxtrash.cpp
@@ -1,16 +1,22 @@
 #include "qxtrash.h"
 
-#ifdef Q_OS_WIN32
-#include <Windows.h>
-#include <ShellAPI.h>
-#endif // Q_OS_WIN32
-
-#include <QByteArray>
 #include <QDebug>
+
+#ifdef Q_OS_MAC
+#include <QByteArray>
 #include <QProcess>
 #include <QStringList>
+#endif
 
-bool QxTrash::moveToTrash(const QString &filePath) {
+#ifdef Q_OS_WIN
+// Note: Also add relevant lib to .pro file:
+// LIBS += -L"C:/Program Files/Microsoft SDKs/Windows/v7.0A/Lib/" -lshell32
+#include <Windows.h>
+#include <ShellAPI.h>
+#include <string>
+#endif // Q_OS_WIN32
+
+bool QxTrash::moveToTrash(const QString &filePath) {    
 #ifdef Q_OS_MAC
 	QString aScript = "tell application \"Finder\" to move POSIX file \"" + filePath + "\" to trash";
 	QString osascript = "/usr/bin/osascript";
@@ -29,10 +35,30 @@ bool QxTrash::moveToTrash(const QString &filePath) {
 		qWarning() << QString("Exit code %1 when deleting file %2: %3").arg(QString::number(p.exitCode()), filePath, resultAsString);
 		return false;
 	}
-#elif Q_OS_WIN32
+#elif defined(Q_OS_WIN)
+    std::wstring path = filePath.toStdWString();
+    path.append(1, L'\0'); // path string must be double nul-terminated
 
+    SHFILEOPSTRUCT shfos = {};
+    shfos.hwnd = nullptr; // handle to window that will own generated windows, if applicable
+    shfos.wFunc = FO_DELETE;
+    shfos.pFrom = path.c_str();
+    shfos.pTo = nullptr; // not used for deletion operations
+    shfos.fFlags = FOF_ALLOWUNDO | FOF_NOCONFIRMATION | FOF_NOERRORUI; // use the recycle bin and display no confirmation box or error messages
+
+    const int retVal = SHFileOperation(&shfos);
+    if (retVal != 0) {
+       if (shfos.fAnyOperationsAborted) {
+          // ...but that's because the user canceled.
+       } else {
+         qWarning() << QString("error when deleting file %1").arg(filePath);
+         return false;
+       }
+    }
 #else
+    Q_UNUSED(filePath);
 	qWarning() << "Platform not supported";
+    return false;
 #endif
 
 	return true;
