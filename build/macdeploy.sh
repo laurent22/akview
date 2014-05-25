3e9808fef91bacd8343a8b52fb327a0f4e38f939
Laurent Cozic <laurent@pogopixels.com>
2014-05-25 03:24:17 -0300

    Fixed dylib dependencies

--- build/macdeploy.sh
+++ build/macdeploy.sh
@@ -10,11 +10,19 @@ echo "Copying own plugins..."
 mkdir -p MultiViewer.app/Contents/PlugIns/multiviewer
 cp ../plugins/release/* MultiViewer.app/Contents/PlugIns/multiviewer
 
+echo "Fixing plugin lib paths..."
+for FILE in MultiViewer.app/Contents/PlugIns/multiviewer/*.dylib; do
+	echo "Processing $FILE..."
+	install_name_tool -change "/usr/local/opt/qt5/lib/QtWidgets.framework/Versions/5/QtWidgets" "@executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets" $FILE
+	install_name_tool -change "/usr/local/opt/qt5/lib/QtGui.framework/Versions/5/QtGui" "@executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui" $FILE
+	install_name_tool -change "/usr/local/opt/qt5/lib/QtCore.framework/Versions/5/QtCore" "@executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore" $FILE
+done
+
+echo "Copying FreeImage lib..."
+cp "/usr/local/lib/libfreeimage.dylib" MultiViewer.app/Contents/PlugIns/multiviewer/
+install_name_tool -change "/usr/local/lib/libfreeimage.dylib" "@executable_path/libfreeimage.dylib" $FILE
+
 echo "Copying version info..."
 cp $SCRIPT_DIR/version/Info.plist MultiViewer.app/Contents
 
 open .
-
-# echo "Moving to Applications folder..."
-# rm -rf /Applications/MultiViewer.app
-# mv MultiViewer.app /Applications/
