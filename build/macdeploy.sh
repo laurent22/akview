9e70949e045d8cbea6f8fcbace324fae025d91e7
Laurent Cozic <laurent@pogopixels.com>
2014-05-25 03:55:16 -0300

    Cleaned-up macdeploy script

--- build/macdeploy.sh
+++ build/macdeploy.sh
@@ -1,28 +1,54 @@
 #!/bin/bash
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd $SCRIPT_DIR/../build-MultiViewer-Qt_5_2_1-Release
+FREEIMAGE_LIB_PATH=/usr/local/lib/libfreeimage.dylib
+QT5_LIB_BASE_PATH=/usr/local/opt/qt5/lib
+MACDEPLOYQT_PATH=/usr/local/Cellar/qt5/5.2.1/bin/macdeployqt
+APP_RELEASE_DIR=$SCRIPT_DIR/../build-MultiViewer-Qt_5_2_1-Release
+
+function updateLibPath {
+	if [[ -n "$(otool -L $1 | grep $2)" ]]; then
+		echo "Updating $2 path..."
+		install_name_tool -change "$3" "$4" $1
+	fi
+}  
+
+cd $APP_RELEASE_DIR
+
+if [[ ! -d MultiViewer.app ]]; then
+	echo "Cannot find MultiViewer.app"
+	exit 1	
+fi
 
 echo "Running macdeployqt..."
-/usr/local/Cellar/qt5/5.2.1/bin/macdeployqt MultiViewer.app -qmldir=/Users/laurent/Docs/PROGS/C++/mv/src
+$MACDEPLOYQT_PATH MultiViewer.app -qmldir=$SCRIPT_DIR/../src
 
 echo "Copying own plugins..."
 mkdir -p MultiViewer.app/Contents/PlugIns/multiviewer
 cp ../plugins/release/* MultiViewer.app/Contents/PlugIns/multiviewer
 
+echo "Copying FreeImage lib..."
+cp "$FREEIMAGE_LIB_PATH" MultiViewer.app/Contents/PlugIns/multiviewer/
+
 echo "Fixing plugin lib paths..."
 for FILE in MultiViewer.app/Contents/PlugIns/multiviewer/*.dylib; do
+	if [[ -n "$(echo $FILE | grep libfreeimage.dylib)" ]]; then
+		echo "Skipping $FILE..."
+		continue
+	fi
+	
 	echo "Processing $FILE..."
-	install_name_tool -change "/usr/local/opt/qt5/lib/QtWidgets.framework/Versions/5/QtWidgets" "@executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets" $FILE
-	install_name_tool -change "/usr/local/opt/qt5/lib/QtGui.framework/Versions/5/QtGui" "@executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui" $FILE
-	install_name_tool -change "/usr/local/opt/qt5/lib/QtCore.framework/Versions/5/QtCore" "@executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore" $FILE
+	
+	updateLibPath $FILE "QtWidgets" "$QT5_LIB_BASE_PATH/QtWidgets.framework/Versions/5/QtWidgets" "@executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets"
+	updateLibPath $FILE "QtGui" "$QT5_LIB_BASE_PATH/QtGui.framework/Versions/5/QtGui" "@executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui"
+	updateLibPath $FILE "QtCore" "$QT5_LIB_BASE_PATH/QtCore.framework/Versions/5/QtCore" "@executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore"
+	updateLibPath $FILE "libfreeimage.dylib" "$FREEIMAGE_LIB_PATH" "@executable_path/libfreeimage.dylib"
 done
-
-echo "Copying FreeImage lib..."
-cp "/usr/local/lib/libfreeimage.dylib" MultiViewer.app/Contents/PlugIns/multiviewer/
-install_name_tool -change "/usr/local/lib/libfreeimage.dylib" "@executable_path/libfreeimage.dylib" $FILE
 
 echo "Copying version info..."
 cp $SCRIPT_DIR/version/Info.plist MultiViewer.app/Contents
 
-open .
+rm -rf ../MultiViewer.app
+mv MultiViewer.app ..
+open ..
