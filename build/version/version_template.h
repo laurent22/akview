b79b04c40be95296b973ea2d56bdb02cbd1e3267
Laurent Cozic <laurent@pogopixels.com>
2014-12-28 20:15:18 +0100

    Update project name

--- build/version/version_template.h
+++ build/version/version_template.h
@@ -7,15 +7,15 @@
 #define VER_PRODUCTVERSION          ____VERSION_COMA____
 #define VER_PRODUCTVERSION_STR      "____VERSION____\0"
 
-#define VER_COMPANYNAME_STR         "MV Project"
-#define VER_FILEDESCRIPTION_STR     "MultiViewer"
-#define VER_INTERNALNAME_STR        "MultiViewer"
-#define VER_LEGALCOPYRIGHT_STR      "Copyright (c) 2014 Laurent Cozic"
+#define VER_COMPANYNAME_STR         "AckView"
+#define VER_FILEDESCRIPTION_STR     "AckView"
+#define VER_INTERNALNAME_STR        "AckView"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright (c) 2014-2015 Laurent Cozic"
 #define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
-#define VER_ORIGINALFILENAME_STR    "MultiViewer.exe"
-#define VER_PRODUCTNAME_STR         "MultiViewer"
-#define VER_DOMAIN_STR              "mv-project.org"
+#define VER_ORIGINALFILENAME_STR    "AckView.exe"
+#define VER_PRODUCTNAME_STR         "AckView"
+#define VER_DOMAIN_STR              "ackview.org"
 
 namespace mv {
 namespace version {
